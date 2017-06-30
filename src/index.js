6cdd47e3be70bd47d2156dc48c4ae3e55c865917
adamakers <adamrakers@gmail.com>
2017-06-29 21:05:04 -0600

    changed project from a Golf App to a football scoreboard

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends Component {
-  
+  constructor() {
+    super(props);
+
+    this.state = {
+      //some state stuff in here
+    }
+  }
   
   render() {
     return (
       <div>
-
+        Hi world
       </div>
     );
   }
 
 }
 
-ReactDOM.render(
-  App, document.querySelector('.container');
-)
+ReactDOM.render(<App />, document.querySelector('.container'));
