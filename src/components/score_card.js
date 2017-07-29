a867d797146d21e232e3cac728608e918f934b5e
adamakers <adamrakers@gmail.com>
2017-07-29 08:18:42 -0600

    few fixes

--- src/components/score_card.js
+++ src/components/score_card.js
@@ -1,38 +1,20 @@
 
 
 
-import React, {Component} from 'react';
+import React from 'react';
 
 
 
-class ScoreCard extends Component {
-
-  constructor(props) {
-    super(props);
-
-    console.log(props);
-
-    this.state = {
-      teamInfo: props.teamInfo
-    }
-
-  }
-
-  //create fn that will determine number of Time Outs team has and then draw it and then call it in render()
-
-  render() {
-    const team = this.state.teamInfo;
-    return (
-      <div className="team">
-        <h2>{(team.name)}</h2>
-        <h3 className="team-score">{(team.score)}</h3>
-      </div>
-    );
-  }
-
+const ScoreCard = (props) => {
+  const team = props.teamInfo
+//create fn that will determine number of Time Outs team has and then draw it and then call it in render()
+  return (
+    <div className="team">
+      <h2>{team.name}</h2>
+      <h3 className="team-score">{team.score}</h3>
+    </div>
+  );
 }
-
-
 
 
 export default ScoreCard;
