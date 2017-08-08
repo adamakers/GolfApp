930119897d99f7dc12f95f023b93c0305a7bf1b2
adamakers <adamrakers@gmail.com>
2017-08-07 21:38:40 -0600

    edited score_btns component.  extra points are disabled by default.  When TD scored, extra points get enabled.

--- src/components/score_btns.js
+++ src/components/score_btns.js
@@ -1,44 +1,73 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import styles from './../styles/score_btn.css';
 
-const ScoreBtns = (props) => {
+
+class ScoreBtns extends Component {
+
+  constructor(props) {
+    super(props);
+
+    //Binding functions
+    this.touchdown = this.touchdown.bind(this);
+    this.fieldgoal = this.fieldgoal.bind(this);
+    this.safety = this.safety.bind(this);
+    this.extraPoint = this.extraPoint.bind(this);
+    this.extraPointTwo = this.extraPointTwo.bind(this);
+
+    //State
+    this.state = {
+      extraPointAttempt: false
+    }
+  }
 
   //touchdown, 6 points
-  const touchdown = (team) => {
-    props.action(6, team);
+  touchdown(team) {
+    this.props.action(6, team);
+    //this.setState({extraPointAttempt: true});
   }
 
   //field Goal, 3 points
-  const fieldgoal = (team) => {
-    props.action(3, team)
+  fieldgoal(team) {
+    this.props.action(3, team)
   }
 
   //safety, 2 points
-  const safety = (team) => {
-    props.action(2, team);
+  safety(team) {
+    this.props.action(2, team);
   }
   
   //extra point, 1 point
-  const extraPoint = (team) => {
-    props.action(1);
+  extraPoint(team) {
+    this.props.action(1, team);
+    //this.setState({extraAttempt: false});
   }
 
   //extraPoint, 2 points
-  const extraPointTwo = (team) => {
-    props.action(2);
+  extraPointTwo(team) {
+    this.props.action(2, team);
+    //this.setState({extraAttempt: false});
   }
 
-  return (
-    <div className="score-btn-container">
-      <button className="score-touchdown" onClick={() => {touchdown(props.team)}}>Touchdown</button> 
-      <button className="score-field-goal" onClick={() => {fieldgoal(props.team)}}>Field Goal</button>
-      <button className="score-safety" onClick={() => {safety(props.team)}}>Safety</button>
-      <button className="score-extra-point-one" onClick={() => {extraPoint(props.team)}} disabled>Extra Point (1)</button>
-      <button className="score-extra-point-two" onClick={() => {extraPointTwo(props.team)}} disabled>Extra Point (2)</button>
-      {/* <button className="score-extra-point-zero" onClick={() => {touchdown(props.team)}} disabled>Missed FG</button> NEED TO CHANGE CALLBACK TO UNLOCK BUTTONS*/}
-    </div>
-  );
-};
+
+
+  render() {
+    return (
+      <div className="score-btn-container">
+        <button className="score-touchdown" onClick={() => {this.touchdown(this.props.team)
+                                                            this.setState({extraPointAttempt: true})}}>Touchdown</button> 
+        <button className="score-field-goal" onClick={() => {this.fieldgoal(this.props.team)}}>Field Goal</button>
+        <button className="score-safety" onClick={() => {this.safety(this.props.team)}}>Safety</button>
+        <button className="score-extra-point-one" onClick={() => {this.extraPoint(this.props.team)
+                                                                  this.setState({extraPointAttempt: false})}} 
+                                                                  disabled={!this.state.extraPointAttempt} >Extra Point (1)</button>
+        <button className="score-extra-point-two" onClick={() => {this.extraPointTwo(this.props.team)
+                                                                  this.setState({extraPointAttempt: false})}}
+                                                                  disabled={!this.state.extraPointAttempt} >Extra Point (2)</button>
+        {/* <button className="score-extra-point-zero" onClick={() => {touchdown(props.team)}} disabled>Missed FG</button> NEED TO CHANGE CALLBACK TO UNLOCK BUTTONS*/}
+      </div>
+    )
+  };
+}
 
 export default ScoreBtns;
